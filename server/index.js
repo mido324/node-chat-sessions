2cf47286289046961d54db1dec4820e2a9e41bc3
mido <mido.ghanim@gmail.com>
2017-12-19 13:58:47 -0700

    completed step 4

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ require('dotenv').config();
 const app = express();
 const session = require('express-session');
 const createInitialSession = require('./middlewere/session')
+const filter = require('./middlewere/filter');
+
 
 app.use( bodyParser.json() );
 app.use( express.static( `${__dirname}/../public/build` ) );
@@ -16,6 +18,19 @@ app.use(session({
 }))
 
 app.use(createInitialSession);
+
+app.use((req, res, next) => {
+    const {method} = req;
+    if (method === "POST" || method === "Put") {
+        filter(req, res, next);
+
+    }
+    else {
+        next();
+    }
+
+});
+
 const messagesBaseUrl = "/api/messages";
 app.post( messagesBaseUrl, mc.create );
 app.get( messagesBaseUrl, mc.read );
