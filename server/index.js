8448d8b5a95784ce6e4161d134296514299428be
mido <mido.ghanim@gmail.com>
2017-12-19 12:35:01 -0700

    step 2

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mc = require( `${__dirname}/controllers/messages_controller` );
-
+require('dotenv').config();
 const app = express();
+const session = require('express-session');
+const createInitialSession = require('./middlewere/session')
 
 app.use( bodyParser.json() );
 app.use( express.static( `${__dirname}/../public/build` ) );
+app.use(session({
+    secret: process.env.SECRET_SESSION,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {maxAge: 10000}
+}))
 
+app.use(createInitialSession);
 const messagesBaseUrl = "/api/messages";
 app.post( messagesBaseUrl, mc.create );
 app.get( messagesBaseUrl, mc.read );
